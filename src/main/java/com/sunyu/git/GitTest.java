23623c6c3ca02c25af374b815a6a9e7b74dfc608
Sunyu <Sunyu@atguigu.com>
2022-06-12 16:31:44 +0800

    hot-fix second commit

--- src/main/java/com/sunyu/git/GitTest.java
+++ src/main/java/com/sunyu/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git7");
-
+        System.out.println("hello git8");
+        System.out.println("hello git9");
     }
 }
