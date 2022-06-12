ae92d6b848a55e2df8fd4651045e8ea94b55fb88
sy <99816782+sunyoona@users.noreply.github.com>
2022-06-12 16:34:18 +0800

    pull commit

--- src/main/java/com/sunyu/git/GitTest.java
+++ src/main/java/com/sunyu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git7");
         System.out.println("hello git8");
         System.out.println("hello git9");
+        System.out.println("pull test");
     }
 }
